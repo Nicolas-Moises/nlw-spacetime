991fd696f8595b56efcb9fe4c1ab43e16d6da708
Nicolas Moises <nicolas.nicc21@gmail.com>
2023-05-16 01:09:13 -0300

    Feat. Setup of application, node, next and react-native

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen bg-zinc-950">
+      <h1 className="text-3xl font-bold text-zinc-50">Hello world</h1>
+    </main>
+  )
+}
