65e8bcf1e1ea365554b7246fc2b486ea6f764f13
Nicolas Moises <nicolas.nicc21@gmail.com>
2023-05-18 01:20:40 -0300

    Feat. Creating auth routes on the server and config JWT token to flow. Implement next auth on the Front end to login social with GitHub

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <footer className="text-xs text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        href="#"
+        target="_blank"
+        rel="noreferrer"
+        className="underline underline-offset-4 hover:text-gray-50"
+      >
+        Nicolas Moises
+      </a>
+    </footer>
+  )
+}
---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,15 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="max-w-sm text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a
+          className="underline underline-offset-4 transition-colors hover:text-gray-50"
+          href="#"
+        >
+          criar agora!
+        </a>
+      </p>
+    </div>
+  )
+}
