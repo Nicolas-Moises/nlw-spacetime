1d45833bd84cff9d6e77aa1e94abc4125cbdac39
Nicolas Moises <nicolas.nicc21@gmail.com>
2023-05-21 14:41:38 -0300

    feat. finish the application

---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { ChangeEvent, useState } from 'react'
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+  function onMediaSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewUrl = URL.createObjectURL(files[0])
+
+    setPreview(previewUrl)
+  }
+  return (
+    <>
+      <input
+        type="file"
+        className="invisible h-0 w-0"
+        id="midia"
+        accept="image/*"
+        name="coverUrl"
+        onChange={onMediaSelected}
+      />
+
+      {preview && (
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </>
+  )
+}
